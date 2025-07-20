da8691c3500211df6dd1389bbca2c762e36b1170
Ivan Kazakov <129972987+4cyne@users.noreply.github.com>
2025-07-20 15:39:07 +0300

    add assert in capitalize.test.js

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,7 +1,6 @@
 export const capitalize = (str) => {
-  if (str === '') {
-    return 'F'
-  }
+  if (!str.length) return ''
+
   const lowerCaseStr = str.toLowerCase()
   const firstLetter = lowerCaseStr.slice(0, 1).toUpperCase()
   return firstLetter + lowerCaseStr.slice(1, lowerCaseStr.length)
--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,7 @@
+import { strict as assert } from 'node:assert'
 import capitalize from '../src/capitalize.js'
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('ой, что то не так')
-}
-
-if (capitalize('') !== '') {
-  throw new Error('ой, что то не так')
-}
+assert.equal(capitalize('hello'), 'Hello')
+assert.equal(capitalize(''), '')
 
 console.log('тест пройден')
